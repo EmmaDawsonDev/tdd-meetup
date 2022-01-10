4034d6d27dd7721249246fd71900854e499913c6
Emma Dawson <emma.l.dawson@gmail.com>
2022-01-10 20:02:32 +0100

    refactor addMeetup tests

--- src/data/users.ts
+++ src/data/users.ts
@@ -1,6 +1,6 @@
 import { IUser, IUserPassword } from '../models/user'
 
-const users: IUserPassword[] = [
+let users: IUserPassword[] = [
   {
     id: 1,
     name: 'Hannah',
@@ -35,4 +35,35 @@ export const validateUser = (email: string, password: string): IUser | undefined
   return user && user.password === password
     ? { id: user.id, name: user.name, email: user.email, attending: user.attending, meetupOwner: user.meetupOwner }
     : undefined
+}
+
+export const resetUsers = () => {
+  users = [
+    {
+      id: 1,
+      name: 'Hannah',
+      email: 'hannah@gmail.com',
+      password: 'hannahIsBest',
+      attending: [3, 4, 5],
+      meetupOwner: [1, 2],
+    },
+    {
+      id: 2,
+      name: 'Joe',
+      email: 'joe@gmail.com',
+      password: 'joeIsBest',
+      attending: [1],
+      meetupOwner: [3, 4, 5],
+    },
+    {
+      id: 3,
+      name: 'Chris',
+      email: 'chris@gmail.com',
+      password: 'chrisIsBest',
+      attending: [1, 4, 3],
+      meetupOwner: [],
+    },
+    { id: 4, name: 'Sofie', email: 'sofie@gmail.com', password: 'sofieIsBest', attending: [], meetupOwner: [] },
+    { id: 5, name: 'Emma', email: 'emma@gmail.com', password: 'emmaIsBest', attending: [], meetupOwner: [] },
+  ]
 }
--- src/pages/AddMeetup/AddMeetup.integration.test.tsx
+++ src/pages/AddMeetup/AddMeetup.integration.test.tsx
@@ -3,13 +3,13 @@ import userEvent from '@testing-library/user-event'
 import { renderWithRouter } from '../../testing-utils'
 import App from '../../App'
 import { store } from '../../store/store'
-
 import { Provider } from 'react-redux'
-
 import { resetMeetups } from '../../data/meetups'
+import { resetUsers } from '../../data/users'
 
 beforeEach(() => {
   resetMeetups()
+  resetUsers()
 
   renderWithRouter(
     <Provider store={store}>
@@ -29,6 +29,42 @@ const login = (email: string, password: string) => {
   userEvent.click(submitBtn)
 }
 
+const logout = () => {
+  const logoutBtn = screen.queryByRole('button', { name: /log out/i })
+  if (logoutBtn) {
+    userEvent.click(logoutBtn)
+  }
+}
+
+const inputNewMeetup = (
+  title: string,
+  startDate: string,
+  startTime: string,
+  endDate: string,
+  endTime: string,
+  description: string,
+  location: string
+) => {
+  const addMeetupBtn = screen.getByRole('button', { name: '+' })
+  userEvent.click(addMeetupBtn)
+  const titleInput = screen.getByLabelText('Title', { exact: false })
+  const startDateInput = screen.getByLabelText('Start Date', { exact: false })
+  const startTimeInput = screen.getByLabelText('Start Time', { exact: false })
+  const endDateInput = screen.getByLabelText('End Date', { exact: false })
+  const endTimeInput = screen.getByLabelText('End Time', { exact: false })
+  const descriptionInput = screen.getByLabelText('Description', { exact: false })
+  const locationInput = screen.getByLabelText('Location', { exact: false })
+  const addBtn = screen.getByRole('button', { name: 'Add meetup' })
+  userEvent.type(titleInput, title)
+  userEvent.type(startDateInput, startDate)
+  userEvent.type(startTimeInput, startTime)
+  userEvent.type(endDateInput, endDate)
+  userEvent.type(endTimeInput, endTime)
+  userEvent.type(descriptionInput, description)
+  userEvent.type(locationInput, location)
+  userEvent.click(addBtn)
+}
+
 describe('Add meetup integration tests', () => {
   it('adds a meetup to the list', () => {
     const currentMeetupList = screen.getAllByTestId('currentListItem')
@@ -36,55 +72,19 @@ describe('Add meetup integration tests', () => {
     // Login
     login('hannah@gmail.com', 'hannahIsBest')
     // Add meetup
-    const addMeetupBtn = screen.getByRole('button', { name: '+' })
-    userEvent.click(addMeetupBtn)
-    const titleInput = screen.getByLabelText('Title', { exact: false })
-    const startDateInput = screen.getByLabelText('Start Date', { exact: false })
-    const startTimeInput = screen.getByLabelText('Start Time', { exact: false })
-    const endDateInput = screen.getByLabelText('End Date', { exact: false })
-    const endTimeInput = screen.getByLabelText('End Time', { exact: false })
-    const descriptionInput = screen.getByLabelText('Description', { exact: false })
-    const locationInput = screen.getByLabelText('Location', { exact: false })
-    const addBtn = screen.getByRole('button', { name: 'Add meetup' })
-    userEvent.type(titleInput, 'New Meetup')
-    userEvent.type(startDateInput, '2022-03-06')
-    userEvent.type(startTimeInput, '18:00')
-    userEvent.type(endDateInput, '2022-03-06')
-    userEvent.type(endTimeInput, '20:00')
-    userEvent.type(descriptionInput, 'This is a description')
-    userEvent.type(locationInput, 'Nyköping')
-    userEvent.click(addBtn)
+    inputNewMeetup('New Meetup', '2022-03-06', '18:00', '2022-03-06', '20:00', 'This is a description', 'Nyköping')
     const updatedCurrentMeetupList = screen.getAllByTestId('currentListItem')
     expect(updatedCurrentMeetupList).toHaveLength(4)
-    const logoutBtn = screen.getByRole('button', { name: /log out/i })
-    userEvent.click(logoutBtn)
+    logout()
   })
   it('shows an error if the meetup date has already passed', () => {
     // Login
     login('hannah@gmail.com', 'hannahIsBest')
     // Add meetup
-    const addMeetupBtn = screen.getByRole('button', { name: '+' })
-    userEvent.click(addMeetupBtn)
-    const titleInput = screen.getByLabelText('Title', { exact: false })
-    const startDateInput = screen.getByLabelText('Start Date', { exact: false })
-    const startTimeInput = screen.getByLabelText('Start Time', { exact: false })
-    const endDateInput = screen.getByLabelText('End Date', { exact: false })
-    const endTimeInput = screen.getByLabelText('End Time', { exact: false })
-    const descriptionInput = screen.getByLabelText('Description', { exact: false })
-    const locationInput = screen.getByLabelText('Location', { exact: false })
-    const addBtn = screen.getByRole('button', { name: 'Add meetup' })
-    userEvent.type(titleInput, 'New Meetup')
-    userEvent.type(startDateInput, '2021-03-06')
-    userEvent.type(startTimeInput, '18:00')
-    userEvent.type(endDateInput, '2021-03-06')
-    userEvent.type(endTimeInput, '20:00')
-    userEvent.type(descriptionInput, 'This is a description')
-    userEvent.type(locationInput, 'Nyköping')
-    userEvent.click(addBtn)
+    inputNewMeetup('New Meetup', '2021-03-06', '18:00', '2021-03-06', '20:00', 'This is a description', 'Nyköping')
     const errorMessage = screen.getByText('Please choose a future date')
     expect(errorMessage).toBeInTheDocument()
-    const logoutBtn = screen.getByRole('button', { name: /log out/i })
-    userEvent.click(logoutBtn)
+    logout()
   })
   it('adds a meetup to the list in the correct order', () => {
     const currentMeetupList = screen.getAllByTestId('currentListItem')
@@ -92,31 +92,13 @@ describe('Add meetup integration tests', () => {
     // Login
     login('hannah@gmail.com', 'hannahIsBest')
     // Add meetup
-    const addMeetupBtn = screen.getByRole('button', { name: '+' })
-    userEvent.click(addMeetupBtn)
-    const titleInput = screen.getByLabelText('Title', { exact: false })
-    const startDateInput = screen.getByLabelText('Start Date', { exact: false })
-    const startTimeInput = screen.getByLabelText('Start Time', { exact: false })
-    const endDateInput = screen.getByLabelText('End Date', { exact: false })
-    const endTimeInput = screen.getByLabelText('End Time', { exact: false })
-    const descriptionInput = screen.getByLabelText('Description', { exact: false })
-    const locationInput = screen.getByLabelText('Location', { exact: false })
-    const addBtn = screen.getByRole('button', { name: 'Add meetup' })
-    userEvent.type(titleInput, 'New Meetup')
-    userEvent.type(startDateInput, '2022-12-26')
-    userEvent.type(startTimeInput, '18:00')
-    userEvent.type(endDateInput, '2022-12-26')
-    userEvent.type(endTimeInput, '20:00')
-    userEvent.type(descriptionInput, 'This is a description')
-    userEvent.type(locationInput, 'Nyköping')
-    userEvent.click(addBtn)
+    inputNewMeetup('New Meetup', '2022-12-26', '18:00', '2022-12-26', '20:00', 'This is a description', 'Nyköping')
     const updatedCurrentMeetupList = screen.getAllByTestId('currentListItem')
     expect(updatedCurrentMeetupList).toHaveLength(4)
     expect(updatedCurrentMeetupList[0]).toContainHTML('<p><strong>Start: </strong>Sat Dec 17 2022 13:00</p>')
     expect(updatedCurrentMeetupList[1]).toContainHTML('<p><strong>Start: </strong>Mon Dec 26 2022 18:00</p>')
     expect(updatedCurrentMeetupList[2]).toContainHTML('<p><strong>Start: </strong>Tue Dec 27 2022 13:00</p>')
     expect(updatedCurrentMeetupList[3]).toContainHTML('<p><strong>Start: </strong>Sat Jan 07 2023 19:30</p>')
-    const logoutBtn = screen.getByRole('button', { name: /log out/i })
-    userEvent.click(logoutBtn)
+    logout()
   })
 })
